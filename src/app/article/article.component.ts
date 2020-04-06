61778146e5e9feb5739999c37a732937b411af5c
Aleksandr Zaviazkin <hyrotron@yandex.ru>
2020-04-06 18:43:50 +0300

    Create an array of Articles, add Input decorator to pass [article] parameter in view, iterate the array and show all articles.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from "@angular/core";
+import { Component, OnInit, HostBinding, Input } from "@angular/core";
 import { Article } from "./article.model"; // import our model class to store the properties of each article
 
 @Component({
@@ -8,21 +8,19 @@ import { Article } from "./article.model"; // import our model class to store th
 })
 export class ArticleComponent implements OnInit {
   @HostBinding("attr.class") cssClass = "row";
-  article: Article;
+  // Add @Input decorator to pass [article] parameter in app.component.html
+  @Input() article: Article;
 
-  constructor() {    
-    this.article = new Article(
-      'Angular 7',
-      'https://angular.io',
-      9
-    );
+  constructor() {
+    // article is populated by the Input now,
+    // so we don't need anything here
   }
 
   voteUp() {
     this.article.voteUp();
     return false;
   }
-  
+
   voteDown() {
     this.article.voteDown();
     return false;
