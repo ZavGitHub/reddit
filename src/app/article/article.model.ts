9b04c9be22c9f471c657accafa6b67cb8b913b7a
Aleksandr Zaviazkin <hyrotron@yandex.ru>
2020-04-06 13:52:53 +0300

    Move voting methods from Controller(Component) to the Model, add domain() function

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -5,9 +5,30 @@ export class Article {
   votes: number;
   title: string;
   link: string;
-  constructor(title: string, link: string, votes?: number) { // votes? means that this parameter is optional
+  // votes? means that this parameter is optional
+  constructor(title: string, link: string, votes?: number) {
     this.title = title;
     this.link = link;
     this.votes = votes || 0; // default value is zero
+  }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  // utility function that extracts the domain from URL
+  domain(): string {
+    try {
+      // e.g. https://google.com/path/to/home
+      const domainAndPath: string = this.link.split("//")[1];
+      // e.g. google.com/path/to/home
+      return domainAndPath.split("/")[0];
+    } catch (err) {
+      return null;
+    }
   }
 }
