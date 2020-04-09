873f5788af637dae57ab889399af51df35d4972c
azavyazk <Alexander.Zavyazkin@t-systems.com>
2020-04-09 17:22:56 +0300

    Adding new article by submit button and sort the list by voting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,14 @@ export class AppComponent {
     ];
   }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article(title.value, link.value));
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    title.value = '';
+    link.value = '';
     return false;
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a. votes);
   }
   title = 'angular-reddit';
 }
